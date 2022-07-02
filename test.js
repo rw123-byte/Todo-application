537482689a5919140880329cce2bfd647203ee7b
ruthwekesa <wekesaruth569@gmail.com>
2022-07-03 00:07:50 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,30 @@
+const Todos = [{
+        id: "5",
+        timestamp: "2022-7-2 10:23",
+        body: "Play a game",
+        status: "Not complete",
+    },
+    {
+        id: "2",
+        timestamp: "2022-7-2 11:23",
+        body: "Read novel",
+        status: "Not complete",
+    },
+    {
+        id: "4",
+        timestamp: "2022-7-2 6:26",
+        body: "Watch tutorial",
+        status: "Not complete",
+    },
+    {
+        id: "3",
+        timestamp: "2022-7-2 8:30",
+        body: "Do the assignment",
+        status: "Not complete",
+    },
+];
+
+
+for (let key in Todos) {
+    console.log(key)
+}
